a82106ad5389b63068dc9d34e794cd207834a9c1
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-01-08 07:18:38 +0500

    Auth

---
+++ src/test/java/AuthX/Authorization_Service/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package AuthX.Authorization_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
