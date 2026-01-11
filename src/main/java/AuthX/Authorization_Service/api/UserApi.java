33f84130511087048554124682b244a20c869759
LinKord Azamov <muzaffarazamov007@gmail.com>
2026-01-11 09:59:14 +0500

    Added UserApi methods getCurrentUser and getByEmail

--- src/main/java/AuthX/Authorization_Service/api/UserApi.java
+++ src/main/java/AuthX/Authorization_Service/api/UserApi.java
@@ -1,13 +1,15 @@
 package AuthX.Authorization_Service.api;
 
+import AuthX.Authorization_Service.dto.UserRequestDto;
 import AuthX.Authorization_Service.dto.UserResponseDto;
 import AuthX.Authorization_Service.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,4 +22,26 @@ public class UserApi {
     public List<UserResponseDto> get(){
         return service.get();
     }
+
+    @GetMapping("/{userId}")
+    public UserResponseDto getById(@PathVariable("userId")UUID id){
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> created(@RequestBody UserRequestDto request){
+        UserResponseDto created = service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping("/email/{email}")
+    public UserResponseDto getByEmail(@PathVariable String email){
+        return service.getByEmail(email);
+    }
+
+    @GetMapping("/me")
+    public UserResponseDto me(){
+        return service.getCurrentUser();
+    }
+
 }
